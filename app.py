5a74d7b603ba90ccf9e67e5eb8f83193041d5743
vilasboas73 <vilasboasedsongp@gmail.com>
2026-03-24 22:17:03 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "OK FUNCIONANDO 🚀"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
